a895010bac795d1ec1dad93498f879c56b81b348
Kelly Walsh <91081239+kemwalsh@users.noreply.github.com>
2023-05-08 15:33:53 -0600

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import "./styles.css";
+import { Tree } from "react-arborist";
+import { data } from "./data";
+
+function Node({ node, style, dragHandle }: NodeRendererProps<any>) {
+  const updatedStyle = {
+    ...style
+  };
+  console.log(style);
+
+  return (
+    <div style={updatedStyle} ref={dragHandle} onClick={() => node.toggle()}>
+      <div style={{ border: "1px solid" }}>
+        {node.isLeaf ? "👷🏻‍♀️" : "👷🏼"} {node.data.name}
+      </div>
+    </div>
+  );
+}
+
+export default function App() {
+  return (
+    <div className="App">
+      <Tree className="node" initialData={data} rowHeight={35}>
+        {Node}
+      </Tree>
+    </div>
+  );
+}
---
+++ src/data.ts
@@ -0,0 +1,22 @@
+export const data = [
+  { id: "1", name: "Asset 1" },
+  { id: "2", name: "Asset 2" },
+  {
+    id: "3",
+    name: "Asset Level 1",
+    children: [
+      { id: "c1", name: "Assettttttz" },
+      { id: "c2", name: "Assettttttz" },
+      { id: "c3", name: "Assettttttz" }
+    ]
+  },
+  {
+    id: "4",
+    name: "Assets 3",
+    children: [
+      { id: "d1", name: "Bassetz" },
+      { id: "d2", name: "Cassetz" },
+      { id: "d3", name: "Dassetz" }
+    ]
+  }
+];
